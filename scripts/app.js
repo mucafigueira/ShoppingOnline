c2b2089c18a68486c4790ab8d0708d712673a9a5
aguinaldomuca <aguinaldomuca@gmail.com>
2026-03-03 20:46:35 +0100

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById("menu-button");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("show-menu");
+});
